f20315f37d27a232b68dcfd587c6a6ec3851b0b0
Yurii19870524 <116317088+Yurii19870524@users.noreply.github.com>
2023-04-10 11:36:44 +0200

    modified:   package-lock.json         modified:   package.json         modified:   src/js/01-gallery.js         modified:   src/js/02-video.js         modified:   src/js/03-feedback.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery_item" href="${original}"><img class="gallery_image" src="${preview}" alt="${description}"/></a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
